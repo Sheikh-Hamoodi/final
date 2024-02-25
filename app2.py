33ec0630b9246c44f64d31c4351f877638692ca5
HamoodiHamood <137614996+Sheikh-Hamoodi@users.noreply.github.com>
2024-02-25 22:12:07 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,11 +4,15 @@ from streamlit_option_menu import option_menu  # pip install streamlit-option-me
 import plotly.graph_objects as go
 from datetime import date, datetime
 import calendar
+from deta import Deta
 import database as db
 from database import insert_period, fetch_all_periods, get_period, water_simulation, get_water_drank
 
 my_date = date.today()
 today = calendar.day_name[my_date.weekday()]
+DETA_KEY = "a0wq3cud5fq_gE78mR9NwdMaBhMQV2nUm7dfQ2VtgyGy"
+deta = Deta(DETA_KEY)
+db = deta.Base("hydration-system")
 
 # -------------- SETTINGS --------------
 personals = ["Height", "Weight", "Age"]
@@ -46,7 +50,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
-    options=["Data Entry", "Data Visualization"],
+    options=["Data Entry", "Model"],
     icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
 )
@@ -64,7 +68,7 @@ if selected == "Data Entry":
             for i, item in enumerate(personals):
                 st.number_input(f"{item}:", min_value=0, format="%i", step=1, key=f"{item}_{i}")
         
-        with st.expander("Diet (in grams)"):
+        with st.expander("Diet"):
             for j, diet in enumerate(diets):
                 st.number_input(f"{diet}:", min_value=0, format="%i", step=10, key=f"{diet}_{j}")
 
@@ -82,14 +86,14 @@ if selected == "Data Entry":
 
 
 # --- PLOT PERIODS ---
-if selected == "Data Visualization":
+if selected == "Model":
     days = ['Friday', 'Monday', 'Saturday', 'Sunday', 'Thursday', 'Tuesday', 'Wednesday']
     today_index = days.index(today)
 
     water_goal = water_simulation()
     water_drank = get_water_drank()
 
-    difference = water_drank[today_index] - water_drank[today_index-1]
+    
 
     #water_goal = days_sorted(water_goal1)
     #water_drank = days_sorted(water_drank1)
@@ -115,12 +119,60 @@ if selected == "Data Visualization":
 
     #Display the plot in Streamlit
     st.plotly_chart(fig)
+
+    data = []
+    res = db.fetch()
+    data_full = res.items
+    print(data_full)
+    for entry in data_full:
+        if entry['key'].lower() == today.lower():
+            data.append(entry)
+
     
-    st.write("**Recommendation**: Drink more water")
+    caffeine_intake = data[0]['Diet']['Caffeine']
+    age = data[0]['Personals']['Age']
+    exercise_level = data[0]['Exercise Intensity']
+
+    age_group = "children" if age <= 12 else "adolescents" if age <= 18 else "adults"
+    recommendations = {"children": 45, "adolescents": 85, "adults": 400}
+
+    water_deviation = water_drank[today_index] - water_drank[today_index-1]
+    caffeine_deviation = caffeine_intake - recommendations[age_group]
+    today_water = (water_goal[today_index] - water_drank[today_index])/100
+
 
     left_column, middle_column, right_column = st.columns(3)
     with left_column:
-        st.subheader("Water Drank")
-        st.metric(label="Water drank today", value=f"{water_drank[today_index]}ml", label_visibility="collapsed", delta=f"{difference}ml from yesterday")
+        st.subheader("**:blue[Water Drank:]**")
+        st.metric(label="water", value=f"{water_drank[today_index]}ml", label_visibility="collapsed", delta=f"{water_deviation}ml from yesterday")
 
+    with middle_column:
+        st.subheader("**:blue[Caffeine:]**")
+        st.metric(label="caffeine", value=f"{caffeine_intake}mg", label_visibility="collapsed", delta=f"{caffeine_deviation}mg from your age limit", delta_color="inverse")
 
+    with right_column:
+        st.subheader("**:blue[Excercise:]**")
+        st.metric(label="excercise", value=f"{exercise_level}/10", label_visibility="collapsed", delta=f"{exercise_level-5} from average (5)")
+
+    yesterday_difference = water_drank[today_index-1] - water_goal[today_index-1]
+    percentage = round((water_drank[today_index-1] / water_goal[today_index-1]) * 100, 0)
+    extra_needed = round(abs(yesterday_difference)/100, 0)
+
+    #Recomendation/Prediction
+    st.subheader("**:blue[Recommendation/Prediction:]**")
+    
+    if today_water>0:
+        st.write(f"**{str(round(int(today_water),0))}** cups left for today.")
+    if percentage >= 100:
+        st.write("You drank enough water yesterday. Keep up the good work!")
+    else:
+        st.write(f"You drank only **{percentage}%** of your daily goal yesterday.")
+        if percentage >= 75:
+            st.write(f"You might experience: **Slight dehydration**.")
+        elif percentage >= 50:
+            st.write(f"You might experience: **tiredness**.")
+        else:
+            st.write(f"You may experience: **fatigue**.")
+        st.write(f"Try to drink **{int(extra_needed)}** cups of water today to compensate.")
+
+    st.write(data)
