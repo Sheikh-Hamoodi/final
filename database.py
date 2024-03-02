7144b71a38fb478d2f9365253922399fd8c0aadd
HamoodiHamood <137614996+Sheikh-Hamoodi@users.noreply.github.com>
2024-03-02 23:02:48 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,28 +1,22 @@
-import streamlit as st  # pip install streamlit
-from deta import Deta  # pip install deta
-#from serial import Serial
-from datetime import date, datetime
+import streamlit as st
+from deta import Deta
+from datetime import date
 import calendar
-import time
 
-
-# Get today's date
+# Today
 my_date = date.today()
 day = calendar.day_name[my_date.weekday()]
 
-# Load the environment variables
+# Initialise database
 DETA_KEY = "a0wq3cud5fq_gE78mR9NwdMaBhMQV2nUm7dfQ2VtgyGy"
-
-# Initialize with a project key
 deta = Deta(DETA_KEY)
-
-# This is how to create/connect a database
 db = deta.Base("hydration-system")
 
 
+# The following functions are used by app.py, and are kept in a seperate file for organisation
+
 def insert_period(day, intensity_value, personal_data, diet_data, default_temp, init_water_drank):
     return db.put({"key": day, "Exercise Intensity": intensity_value, "Personals": personal_data, "Diet": diet_data, "Temperature": default_temp, "Water Drank": init_water_drank})
-
 
 def fetch_all_periods():
     res = db.fetch()
@@ -39,10 +33,12 @@ def get_period(day):
 
     return db.get(day)
 
+
+# What if Q1
 def water_simulation():
 
     # Constants
-    AVG_SWEAT_RATE = 0.8 # liters per hour
+    AVG_SWEAT_RATE = 0.3 # liters per hour
     URINE_OUTPUT = 1.5 # liters per day
     FIBER_MULTIPLIER = 0.1 # liters per 10 grams of fiber
     SALT_MULTIPLIER = 0.6 # liters per 5 grams of salt
@@ -63,25 +59,23 @@ def water_simulation():
         # Inputs
         env_temp = data[0]['Temperature'] # degrees Celsius
         exercise_level = data[0]['Exercise Intensity']
-
         age = data[0]['Personals']['Age'] # years
         height = data[0]['Personals']['Height'] # cm
         weight = data[0]['Personals']['Weight'] # kg
-
         caffeine_intake = data[0]['Diet']['Caffeine'] # mg
         daily_fiber = data[0]['Diet']['Fibre'] # grams
         salt_intake = data[0]['Diet']['Salt'] # grams
 
-        # Calculate BMR using Harris-Benedict equation
-        BMR = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age)
+        
+        # Calculations with Validated parameters (note: already validated in app.py)
 
-        # Calculate sweating
+        # BMR using Harris-Benedict equation
+        BMR = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age)
         sweating = AVG_SWEAT_RATE * int(env_temp) * 24
 
-        total_fluid_intake = (BMR * 0.5)  + URINE_OUTPUT + (daily_fiber / 10) * FIBER_MULTIPLIER * 1000 + (salt_intake / 5) * SALT_MULTIPLIER * 1000 - (caffeine_intake / 100) * CAFFEINE_MULTIPLIER * 1000
-
+        total_fluid_intake = (BMR * 0.5)  + URINE_OUTPUT + (daily_fiber / 10) * FIBER_MULTIPLIER * 1000 + (salt_intake / 5) * SALT_MULTIPLIER * 1000 - (caffeine_intake / 100) * CAFFEINE_MULTIPLIER * 1000 + sweating
         total_fluid_intake += exercise_level*100
 
-        #print("Total fluid intake needed:", total_fluid_intake, "ml/day")
         results.append(total_fluid_intake)
+
     return results
